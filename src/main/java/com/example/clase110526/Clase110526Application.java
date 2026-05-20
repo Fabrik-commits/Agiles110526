48b1b456bc23aabab697cf1aa1230a19853cd122
Fabrik-commits <fabriknapo@gmail.com>
2026-05-20 11:47:44 -0300

    mi primer commit

---
+++ src/main/java/com/example/clase110526/Clase110526Application.java
@@ -0,0 +1,13 @@
+package com.example.clase110526;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase110526Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase110526Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clase110526/Clase110526ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clase110526;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase110526ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
